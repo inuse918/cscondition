785e5fa23eb4d51326a3d5d9698e900ad13ea52d
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 11:25:16 +0900

    오전과 오후 구분

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,15 +10,32 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("숫자 입력: ");
+            Console.Write("숫자 입력: ");
             int input = int.Parse(Console.ReadLine());
             if (input % 2 == 0)
             {
-                Console.Write("짝수 입니다!");
+                Console.WriteLine("짝수 입니다!");
             }
-            if (input %2 == 1)
+            else
             {
-                Console.Write("홀수 입니다!");
+                Console.WriteLine("홀수 입니다!");
+            }
+            //현재 시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            // 오전과 오후 구분
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다.");
+            }
+            else 
+            {
+                Console.WriteLine("오후입니다.");
             }
         }
     }
