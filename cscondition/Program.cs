16f825835755f15faa2307502765eb1268fd3a1c
inuse918 <s2018s34@e-mirim.hs.kr>
2020-05-06 10:46:35 +0900

    홀수 짝수를 판단

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,6 +10,16 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("숫자 입력: ");
+            int input = int.Parse(Console.ReadLine());
+            if (input % 2 == 0)
+            {
+                Console.Write("짝수 입니다!");
+            }
+            if (input %2 == 1)
+            {
+                Console.Write("홀수 입니다!");
+            }
         }
     }
 }
